236b6d0bea15a01e65abb55e0ac990a0b0387f95
陈威彪 <cwb2@meitu.com>
2020-03-23 00:02:05 +0800

    【feature】提交作业0

---
+++ Ilovegraphics/Ilovegraphics/Assignment1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Assignment1
+//
+//  Created by william on 2020/3/22.
+//  Copyright © 2020 william. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ Ilovegraphics/Ilovegraphics/Ilovegraphics/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Ilovegraphics
+//
+//  Created by william on 2020/3/22.
+//  Copyright © 2020 william. All rights reserved.
+//
+#include<cmath>
+#include<eigen3/Eigen/Core>
+#include<eigen3/Eigen/Dense>
+//#include <opencv2/opencv.hpp>
+#include<iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
